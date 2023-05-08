f60444b8bfb18b96d06be2e0830314d36762fe82
ahmedhesi <shiikh63@gmail.com>
2023-05-08 11:40:12 -0500

    just commiting

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*----- constants -----*/
+
+
+	/*----- state variables -----*/
+	let secretWord;
+	let results;
+	let correctGuess;
+	
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
+
