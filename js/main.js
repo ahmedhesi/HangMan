d9ac12db9bd90053962c6764df3fd6031e0e15b8
ahmedhesi <shiikh63@gmail.com>
2023-05-10 00:16:49 -0500

    did intilize function

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 const WORD_BANK= [
     "METEOR",
     "SHOOTINGSTAR",
-    "MILKYWAY",
+    "MILKY WAY",
     "GALAXY",
     "PLUTO",
 ]
 
-Const MAX_WRONG_GUESSES= 6;
+const MAX_WRONG_GUESSES= 6;
 
 
 
@@ -20,24 +20,37 @@ Const MAX_WRONG_GUESSES= 6;
     let winner;
 	
 /*----- cached elements  -----*/
-
+const resultsEL = document.getElementById('game-result');
+const emptyWordBank = document.querySelector("div");
 
 /*----- event listeners -----*/
+document.getElementById("button").addEventListener("click", handleClick) {
 
+}
 
 /*----- functions -----*/
 
 function initilize() {
-    secretWord = WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)];
-    playerWord = "_"; 
+    secretWord = WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)].toUpperCase().split("");
+    playerWord = secretWord.map(function (ltr){
+       ltr === " " ? " " ; "_"
+        }) 
     wrongGuesses = [];
     winner = undefined;
     render()
 }
 
+function handleClick (evt) {
+    
+}
+
 function render() {
     renderResults() 
-    
 
+
+}
+
+function renderResults() {
+emptyWordBank.innerContent = playerWord.join(" ")
 }
 
