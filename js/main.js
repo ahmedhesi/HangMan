5acefd6cb5f03e1b1ff1f1a6c1f263473e154c16
ahmedhesi <shiikh63@gmail.com>
2023-05-10 15:03:37 -0500

    game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
 const WORD_BANK= [
     "METEOR",
-    "SHOOTINGSTAR",
+    "SHOOTING STAR",
     "MILKY WAY",
     "GALAXY",
     "PLUTO",
@@ -16,50 +16,58 @@ const MAX_WRONG_GUESSES= 6;
 /*----- state variables -----*/
 	let secretWord;
 	let playerWord;
-	let wrongGuesses;
+	let guesses;
     let winner;
+    let guessNum;
 	
 /*----- cached elements  -----*/
 const resultsEL = document.getElementById('game-result');
 const emptyWordBank = document.querySelector("div");
+const guessWordEl = document.getElementById('guess-word');
 
 /*----- event listeners -----*/
-document.getElementById("button").addEventListener("click", handleClick) {
-
-}
+document.getElementById("letters").addEventListener("click", handleClick)
 
 /*----- functions -----*/
-
+initilize()
 function initilize() {
     secretWord = WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)].toUpperCase().split("");
-    playerWord = secretWord.map(function (ltr){
-       ltr === " " ? " " ; "_"
-        }) 
-    wrongGuesses = [];
+    guesses = [];
+    guessNum = 0 
     winner = undefined;
     render()
 }
 
-function handleClick (evt) {
-    const letter = evt.target.innerContent;
-    if (evt.target.tagname !== "BUTTON")  {
-       return
-    }  if () {
-        
-    } else if () {
-        
-    }
-    
-    render()   
+function handleClick (evt) { 
+    const letter = evt.target.innerHTML; 
+    if (guesses.includes(letter) || evt.target.tagName !== "BUTTON") return 
+    guesses.push(letter)
+    if(!secretWord.includes(letter)) guessNum += 1
+    winner = checkWinner();
+    console.log(winner)
+    render() 
 }
 
 function render() {
-    renderResults() 
+    if (guessWordEl.children.length === 0) {
+        secretWord.forEach(letter => {
+          const newDiv = document.createElement("div")
+          if (letter !== " ")  newDiv.classList.add("letter") 
+          guessWordEl.append(newDiv)
+        });
+    }
+    secretWord.forEach((letter, idx) => { 
+        if (guesses.includes(letter)) guessWordEl.children[idx].innerHTML = letter;
 
-
+    });
 }
 
-function renderResults() {
-emptyWordBank.innerContent = playerWord.join(" ")
+function checkWinner(){
+    if (guessNum === 6) return "L"
+    if (secretWord.every(letter => guesses.includes(letter))) return "W"; 
+    return null;
 }
+
+
+
 
