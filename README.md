4df9337b06306b4cb89556d608d080a58bddcfa4
ahmedhesi <shiikh63@gmail.com>
2023-05-12 01:50:16 -0500

    touch up code

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# <Your Project's Title>
-A description of your game. Background info of the game is a nice touch.
+# SpaceMan
+Hangman is a classic word guessing game that has entertained people for generations. The game revolves around solving a mystery word by guessing individual letters.
 
 # Screenshot
 
+<img src="https://i.imgur.com/kL1IwEi.png">
 <img src="url to your image on imgur">
 <img src="url to your image on imgur">
 
--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 /*----- constants -----*/
-const WORD_BANK= [
+const WORD_BANK = [
     "METEOR",
-    "SHOOTING STAR",
-    "MILKY WAY",
+    "STAR",
+    "MILKYWAY",
     "GALAXY",
     "PLUTO",
 ]
 
-const MAX_WRONG_GUESSES= 6;
-
-
-
-
+const MAX_WRONG_GUESSES = 6;
 
 /*----- state variables -----*/
-	let secretWord;
-	let playerWord;
-	let guesses;
-    let winner;
-    let guessNum;
-	
+let secretWord;
+let playerWord;
+let guesses;
+let winner;
+let guessNum;
+
 /*----- cached elements  -----*/
 const imgEL = document.getElementById('game-result');
 const emptyWordBank = document.querySelector("div");
 const guessWordEl = document.getElementById('guess-word');
 const playAgainBtn = document.getElementById('play');
 const guessesRemainingEl = document.getElementById('guess-count');
+const winLoseEl = document.getElementById('win-lose')
 
 /*----- event listeners -----*/
 document.getElementById("letters").addEventListener("click", handleClick);
-playAgainBtn.addEventListener("click", function(){
-    guessWordEl.innerHTML = " " 
+playAgainBtn.addEventListener("click", function () {
+    guessWordEl.innerHTML = " "
     initialize()
 });
 
@@ -40,16 +37,16 @@ function initialize() {
     secretWord = WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)].toUpperCase().split("");
     console.log(secretWord)
     guesses = [];
-    guessNum = 0 
+    guessNum = 0
     winner = undefined;
     render();
 }
 
-function handleClick (evt) { 
-    const letter = evt.target.innerHTML; 
-    if (guesses.includes(letter) || evt.target.tagName !== "BUTTON") return 
+function handleClick(evt) {
+    const letter = evt.target.innerHTML;
+    if (guesses.includes(letter) || evt.target.tagName !== "BUTTON") return
     guesses.push(letter)
-    if(!secretWord.includes(letter)) guessNum += 1
+    if (!secretWord.includes(letter)) guessNum += 1
     winner = checkWinner();
     render();
 }
@@ -57,23 +54,29 @@ function handleClick (evt) {
 function render() {
     if (guessWordEl.children.length === 0) {
         secretWord.forEach(letter => {
-          const newDiv = document.createElement("div")
-          if (letter !== " ")  newDiv.classList.add("letter") 
-          guessWordEl.append(newDiv)
+            const newDiv = document.createElement("div")
+            if (letter !== " ") newDiv.classList.add("letter")
+            guessWordEl.append(newDiv)
         });
     }
-    secretWord.forEach((letter, idx) => { 
+    secretWord.forEach((letter, idx) => {
         if (guesses.includes(letter)) guessWordEl.children[idx].innerHTML = letter;
-
     });
     imgEL.src = `spaceman-images/spaceman-${guessNum}.jpg`
-
     guessesRemainingEl.innerText = `${guessNum}`
+    if (winner === "W") {
+        winLoseEl.innerText = "You Win!";
+    } else if (winner === "L") {
+        winLoseEl.innerText = "You Lose!";
+    } else {
+        winLoseEl.innerText = ""
+    }
+
 }
 
-function checkWinner(){
+function checkWinner() {
     if (guessNum === 6) return "L"
-    if (secretWord.every(letter => guesses.includes(letter))) return "W"; 
+    if (secretWord.every(letter => guesses.includes(letter))) return "W";
     return null;
 }
 
